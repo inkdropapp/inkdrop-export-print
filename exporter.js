8a68823340b53063620812d404993ec09390bf6c
Takuya Matsuyama <hi@craftz.dog>
2019-03-15 13:51:31 +0900

    feat(*): Support inkdrop 4.x

--- exporter.js
+++ exporter.js
@@ -2,87 +2,98 @@ const remote = require('electron').remote
 const path = require('path')
 const fs = require('fs')
 const exportUtils = require('inkdrop-export-utils')
-const dialog = remote.dialog
+const { dialog } = remote
 
 module.exports = {
-  async exportAsPDF () {
-    const { document } = inkdrop.flux.getStore('editor').getState()
-    if (document) {
-      const pathToSave = dialog.showSaveDialog({
-        title: 'Save PDF file',
-        defaultPath: `${document.title}.pdf`,
-        filters: [
-          { name: 'PDF Files', extensions: [ 'pdf' ] },
-          { name: 'All Files', extensions: [ '*' ] }
-        ]
-      })
+  exportAsPDF,
+  print
+}
 
-      if (typeof pathToSave === 'string') {
-        const webView = await this.createWebView(document)
-
-        try {
-          const opts = {
-            pageSize: 'A4',
-            printBackground: true
-          }
-          await new Promise((resolve, reject) => {
-            webView.printToPDF(opts, (error, data) => {
-              if (error) {
-                reject(error)
-              }
-              fs.writeFileSync(pathToSave, data)
-            })
-          })
-        } catch (e) {
-          inkdrop.notifications.addError('Failed to save HTML', e.stack)
-        }
-        this.removeWebView(webView)
-      }
-    } else {
-      inkdrop.notifications.addError('No note opened', { detail: 'Please open a note to export as HTML', dismissable: true })
-    }
-  },
-
-  async print () {
-    const { document } = inkdrop.flux.getStore('editor').getState()
-    if (document) {
-      const webView = await this.createWebView(document)
-      webView.print()
-      this.removeWebView(webView)
-    } else {
-      inkdrop.notifications.addError('No note opened', { detail: 'Please open a note to export as HTML', dismissable: true })
-    }
-  },
-
-  async createWebView (note) {
-    const templateFilePath = require.resolve(path.join('inkdrop-export-utils', 'assets', 'print-template.html'))
-    const templateHtml = fs.readFileSync(templateFilePath, 'utf-8')
-
-    let markdown = `# ${note.title}\n${note.body}`
-    const htmlBody = await exportUtils.renderHTML(markdown)
-    const htmlStyles = exportUtils.getStylesheets()
-    const outputHtml = templateHtml
-      .replace('{%body%}', htmlBody)
-      .replace('{%styles%}', htmlStyles)
-      .replace('{%title%}', note.title)
-    const fn = this.saveHTMLToTmp(outputHtml)
-    const webView = document.createElement('webview')
-    window.document.body.appendChild(webView)
-    global.el = webView
-    webView.src = fn
-    await new Promise((resolve) => {
-      webView.addEventListener('dom-ready', resolve)
+async function exportAsPDF() {
+  const { editingNote } = inkdrop.store.getState()
+  if (editingNote) {
+    const pathToSave = dialog.showSaveDialog({
+      title: 'Save PDF file',
+      defaultPath: `${editingNote.title}.pdf`,
+      filters: [
+        { name: 'PDF Files', extensions: ['pdf'] },
+        { name: 'All Files', extensions: ['*'] }
+      ]
     })
-    return webView
-  },
 
-  removeWebView (webView) {
-    setTimeout(() => window.document.body.removeChild(webView), 30 * 60 * 1000)
-  },
+    if (typeof pathToSave === 'string') {
+      const webView = await createWebView(editingNote)
 
-  saveHTMLToTmp (html) {
-    const fn = path.join(require('os').tmpdir(), 'inkdrop-export.html')
-    fs.writeFileSync(fn, html, 'utf-8')
-    return fn
+      try {
+        const opts = {
+          pageSize: 'A4',
+          printBackground: true
+        }
+        await new Promise((resolve, reject) => {
+          webView.printToPDF(opts, (error, data) => {
+            if (error) {
+              reject(error)
+            }
+            fs.writeFileSync(pathToSave, data)
+          })
+        })
+      } catch (e) {
+        inkdrop.notifications.addError('Failed to save HTML', e.stack)
+      }
+      removeWebView(webView)
+    }
+  } else {
+    inkdrop.notifications.addError('No note opened', {
+      detail: 'Please open a note to export as HTML',
+      dismissable: true
+    })
   }
+}
+
+async function print() {
+  const { editingNote } = inkdrop.store.getState()
+  if (editingNote) {
+    const webView = await createWebView(editingNote)
+    webView.print()
+    removeWebView(webView)
+  } else {
+    inkdrop.notifications.addError('No note opened', {
+      detail: 'Please open a note to export as HTML',
+      dismissable: true
+    })
+  }
+}
+
+async function createWebView(note) {
+  const templateFilePath = require.resolve(
+    path.join('inkdrop-export-utils', 'assets', 'print-template.html')
+  )
+  const templateHtml = fs.readFileSync(templateFilePath, 'utf-8')
+
+  const markdown = `# ${note.title}\n${note.body}`
+  const htmlBody = await exportUtils.renderHTML(markdown)
+  const htmlStyles = exportUtils.getStylesheets()
+  const outputHtml = templateHtml
+    .replace('{%body%}', htmlBody)
+    .replace('{%styles%}', htmlStyles)
+    .replace('{%title%}', note.title)
+  const fn = saveHTMLToTmp(outputHtml)
+  const webView = document.createElement('webview')
+  window.document.body.appendChild(webView)
+  global.el = webView
+  webView.src = fn
+  await new Promise(resolve => {
+    webView.addEventListener('dom-ready', resolve)
+  })
+  return webView
+}
+
+function removeWebView(webView) {
+  setTimeout(() => window.document.body.removeChild(webView), 30 * 60 * 1000)
+}
+
+function saveHTMLToTmp(html) {
+  const fn = path.join(require('os').tmpdir(), 'inkdrop-export.html')
+  fs.writeFileSync(fn, html, 'utf-8')
+  return fn
 }
--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-const remote = require('electron').remote
-const path = require('path')
-const fs = require('fs')
-const exportUtils = require('inkdrop-export-utils')
-const dialog = remote.dialog
-
 module.exports = {
-  activate () {
+  activate() {
     this.subscription = inkdrop.commands.add(document.body, {
       'export-print:export-as-pdf': () => require('./exporter').exportAsPDF(),
       'export-print:print': () => require('./exporter').print()
     })
   },
 
-  deactivate () {
+  deactivate() {
     this.subscription.dispose()
   }
 }
